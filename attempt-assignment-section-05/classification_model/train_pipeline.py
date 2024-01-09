eeaecd6d85b98549d0963071bf36123e77ea9439
Warren Shelley <warren.shelley1@gmail.com>
2024-01-09 21:49:50 +0000

    Update train test split

--- attempt-assignment-section-05/classification_model/train_pipeline.py
+++ attempt-assignment-section-05/classification_model/train_pipeline.py
@@ -1,7 +1,7 @@
 import numpy as np
 from config.core import config
-from pipeline import price_pipe
-from processing.data_manager import load_dataset, save_pipeline
+from pipeline import predict_pipe
+from processing.data_manager import load_raw_data, save_pipeline
 from sklearn.model_selection import train_test_split
 
 
@@ -9,7 +9,7 @@ def run_training() -> None:
     """Train the model."""
 
     # read training data
-    data = load_dataset(file_name=config.app_config.training_data_file)
+    data = load_raw_data(file_name=config.app_config.training_data_file)
 
     # divide train and test
     X_train, X_test, y_train, y_test = train_test_split(
@@ -20,13 +20,12 @@ def run_training() -> None:
         # for reproducibility
         random_state=config.model_config.random_state,
     )
-    y_train = np.log(y_train)
 
     # fit model
-    price_pipe.fit(X_train, y_train)
+    predict_pipe.fit(X_train, y_train)
 
     # persist trained model
-    save_pipeline(pipeline_to_persist=price_pipe)
+    save_pipeline(pipeline_to_persist=predict_pipe)
 
 
 if __name__ == "__main__":
